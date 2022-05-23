4a9aa7e2ac6624e320f58153fd78cc7ebb65f0d5
omid <o.zeynalpour@gmail.com>
2022-05-23 11:02:31 +0200

    add sec to autoasc

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "custom-sg-private" {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    security_groups = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
